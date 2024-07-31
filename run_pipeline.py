50fb34cbe1ff35c059e4d15ace20104b59d82ee3
Saqlain <saqlain2204@gmail.com>
2024-07-31 17:43:39 +0530

    added deployment pipeline

--- run_pipeline.py
+++ run_pipeline.py
@@ -5,3 +5,5 @@ if __name__ == '__main__':
     # Run the training pipeline
     print(Client().active_stack.experiment_tracker.get_tracking_uri())
     training_pipeline(data_path='./data/olist_customers_dataset.csv')
+
+# mlflow ui --backend-store-uri "file:C:\Users\Asif\AppData\Roaming\zenml\local_stores\9445c78b-0009-4fc3-acc0-eff8e888c9dd\mlruns"
